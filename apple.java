e0a63d857b0d18c19b7225d2a13c696360bdfec4
Wakeupyasid <siddhumore602@gmail.com>
2025-02-25 23:06:31 +0530

    dsa

--- apple.java
+++ apple.java
@@ -1,5 +1,20 @@
+import java.util.Arrays;
+
 public class apple {
     public static void main(String[] args) {
-
+        int nums[]={0,1,2,2,3,0,4,2};
+        int val=2;
+        int arr[]=new int[nums.length];
+        int ans=0;
+        for (int i =0;i<nums.length;i++){
+            if (nums[i]!=val){
+                arr[ans]=nums[i];
+                ans++;
+            }
+        }
+        for (int i=0;i<nums.length;i++){
+            nums[i]=arr[i];
+        }
+        System.out.println(ans);
     }
 }
