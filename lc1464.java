1152c2b83db52bdbaa6e86811a0b38cb03cc1546
Wakeupyasid <siddhumore602@gmail.com>
2025-03-02 23:32:13 +0530

    dsa

--- lc1464.java
+++ lc1464.java
@@ -1,14 +1,13 @@
-import java.awt.*;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-public class Prac {
+public class lc1464 {
+//    1464. Maximum Product of Two Elements in an Array
     public static void main(String[] args) {
         int nums[]={1,5,4,5};
         int ans=0;
-        List<Integer>a=new ArrayList<>();
+        List<Integer> a=new ArrayList<>();
         for (int i=0;i< nums.length-1;i++){
             for (int j=i+1;j<nums.length;j++){
                 int k=(nums[i]-1)*(nums[j]-1);
