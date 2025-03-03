fcbce853d18a9bd1034c29641e333ae1085d17e6
Wakeupyasid <siddhumore602@gmail.com>
2025-03-03 16:08:24 +0530

    dsa

--- lc2942.java
+++ lc2942.java
@@ -1,13 +1,13 @@
-import java.awt.*;
-import java.util.*;
+import java.util.ArrayList;
 import java.util.List;
 
-public class Prac {
+public class lc2942 {
+//    2942. Find Words Containing Character
     public static void main(String[] args) {
         String []words={"abc","bcd","aaaa","cbc"};
         char x='a';
         String s= String.valueOf(x);
-        List<Integer>a=new ArrayList<>();
+        List<Integer> a=new ArrayList<>();
         for (int i=0;i< words.length;i++){
             if (words[i].contains(s)){
                 a.add(i);
