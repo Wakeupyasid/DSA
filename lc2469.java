102bc74c5c5cbc072510b46c8ddbc37c5097fc8a
Wakeupyasid <siddhumore602@gmail.com>
2025-03-08 00:21:33 +0530

    dsa

--- lc2469.java
+++ lc2469.java
@@ -1,10 +1,10 @@
-import java.awt.*;
-import java.util.*;
-import java.util.List;
+import java.util.Arrays;
 
-public class Prac {
+public class lc2469 {
+//    2469. Convert the Temperature
     public static void main(String[] args) {
         double celsius=36.50;
         double ans[]={(celsius+273.15),(celsius * 1.80 + 32.00)};
+        System.out.println(Arrays.toString(ans));
     }
 }
