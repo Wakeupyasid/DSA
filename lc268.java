b7bab0e127885374c8947e1130ad7f22cd2e5eb2
Wakeupyasid <siddhumore602@gmail.com>
2025-03-10 23:29:38 +0530

    dsa

--- lc268.java
+++ lc268.java
@@ -1,12 +1,12 @@
-import java.awt.*;
-import java.util.*;
+import java.util.ArrayList;
 import java.util.List;
 
-public class Prac {
+public class lc268 {
+//    268. Missing Number
     public static void main(String[] args) {
         int nums[]={0,1};
         int ans=0;
-        List<Integer>a=new ArrayList<>();
+        List<Integer> a=new ArrayList<>();
         for (int i = 0; i < nums.length; i++) {
             a.add(nums[i]);
         }
