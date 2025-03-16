7a6e3c62ac98f79b25f71e0883af21e20d6de1bb
Wakeupyasid <siddhumore602@gmail.com>
2025-03-16 23:01:35 +0530

    dsa

---
+++ lc12222.java
@@ -0,0 +1,10 @@
+public class lc12222 {
+    public static void main(String[] args) {
+        int  nums[]={1,2,3,4};
+        int ans=0;
+        for (int i=0;i< nums.length;i++){
+            ans=ans+nums[i];
+        }
+        System.out.println(ans);
+    }
+}
--- lc412.java
+++ lc412.java
@@ -1,22 +1,12 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class lc412 {
     //    412. Fizz Buzz
     public static void main(String[] args) {
-        int n = 15;
-        List<String> ans = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            if ((i + 1) % 3 == 0 && (i + 1) % 5 == 0) {
-                ans.add("FizzBuzz");
-            } else if ((i + 1) % 3 == 0 && (i + 1) % 5 != 0) {
-                ans.add("Fizz");
-            } else if ((i + 1) % 3 != 0 && (i + 1) % 5 == 0) {
-                ans.add("Buzz");
-            } else {
-                ans.add(String.valueOf(i + 1));
-            }
-        }
-        System.out.println(ans);
+        int nums[]={1,2,3,3};
+        Arrays.sort(nums);
+
     }
 }
