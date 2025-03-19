fbc8c155758847cd134cf18b62759f750acecbbd
Wakeupyasid <siddhumore602@gmail.com>
2025-03-20 00:20:32 +0530

    dsa

--- Prac.java
+++ Prac.java
@@ -3,16 +3,22 @@ import java.util.*;
 
 public class Prac {
     public static void main(String[] args) {
-        int num=1248;
-        int nno=num;
-        int ans=0;
-        while (num>0){
-            int t=num%10;
-            if (nno%t==0){
-                ans++;
+        int nums[]={-1,1};
+        List<Integer>p=new ArrayList<>();
+        List<Integer>n=new ArrayList<>();
+        for (int i=0;i< nums.length;i++){
+            if (nums[i]>0){
+                p.add(nums[i]);
+            } else if (nums[i]<0) {
+                n.add(nums[i]);
             }
-            num=num/10;
         }
-        System.out.println(ans);
+        int c=0;
+        for (int i=0;i< nums.length;i=i+2){
+            nums[i]=p.get(c);
+            nums[i+1]=n.get(c);
+            c++;
+        }
+        System.out.println(Arrays.toString(nums));
     }
 }
---
+++ lc2149.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class lc2149 {
+    //    2149. Rearrange Array Elements by Sign
+    public static void main(String[] args) {
+        int nums[] = {-1, 1};
+        List<Integer> p = new ArrayList<>();
+        List<Integer> n = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                p.add(nums[i]);
+            } else if (nums[i] < 0) {
+                n.add(nums[i]);
+            }
+        }
+        int c = 0;
+        for (int i = 0; i < nums.length; i = i + 2) {
+            nums[i] = p.get(c);
+            nums[i + 1] = n.get(c);
+            c++;
+        }
+        System.out.println(Arrays.toString(nums));
+    }
+}
