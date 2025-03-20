30cdccac1860dd4feb02574aefe1666532f7e61c
Wakeupyasid <siddhumore602@gmail.com>
2025-03-20 23:20:10 +0530

    dsa

--- lc2442.java
+++ lc2442.java
@@ -1,10 +1,11 @@
-import java.util.ArrayList;
-import java.util.*;
+import java.util.HashSet;
+import java.util.Set;
 
-public class Prac {
+public class lc2442 {
+//    2442. Count Number of Distinct Integers After Reverse Operations
     public static void main(String[] args) {
         int nums[]={2,2,2};
-        Set<Integer>a=new HashSet<>();
+        Set<Integer> a=new HashSet<>();
         for (int i=0;i< nums.length;i++){
             a.add(nums[i]);
             int res=0;
