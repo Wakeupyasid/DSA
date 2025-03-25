6af17e45c2a9571aa2819ff86e7e6a7bd95bc7cf
Wakeupyasid <siddhumore602@gmail.com>
2025-03-25 22:44:10 +0530

    dsa

--- lc2057.java
+++ lc2057.java
@@ -1,7 +1,5 @@
-import java.util.ArrayList;
-import java.util.*;
-
-public class Prac {
+public class lc2057 {
+    // 2057. Smallest Index With Equal Value
     public static void main(String[] args) {
         int nums[]={1,2,3,4,5,6,7,8,9,0};
         int ans=-1;
