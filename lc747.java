bf7d7bc25fb564374e2c5c9092070e0db6b9bdf3
Wakeupyasid <siddhumore602@gmail.com>
2025-03-28 23:16:23 +0530

    dsa

--- lc747.java
+++ lc747.java
@@ -1,9 +1,5 @@
-import java.util.*;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
-
-public class apple {
+public class lc747 {
+    // 747. Largest Number At Least Twice of Others
     public static void main(String[] args) {
         int nums[]={1,2,3,4};
         int m=0;
