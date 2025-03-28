3a3c2fba8d6591640e19985778ef0dcbddb124d6
Wakeupyasid <siddhumore602@gmail.com>
2025-03-28 23:42:29 +0530

    dsa

--- lc2404.java
+++ lc2404.java
@@ -1,9 +1,9 @@
-import java.util.*;
 import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
 
-public class apple {
+public class lc2404 {
+    // 2404. Most Frequent Even Element
     public static void main(String[] args) {
         int nums[]={8267,8727,2649,7359,2142,7824,7399,3424,8868,476,9769,4631,6631,4205,9178,7920,4748,5523,74,5550,516,6448,3475,7867,7041,5896,4382,2066,8101,674,67,9311,4308,1693,4586,3959,2973,2158,5067,9060,2988,1147,1746,9722,8550,9126,3029,1677,4154,5473,6589,5381,1486,8699,792,8349,6773,496,4505,6163,6843,1232,4809,4432,6251,7658,4949,1419,7005,3815,6295,4635,7785,4891,6941,6952,7218,134,2718,7409,9551};
         Arrays.sort(nums);
