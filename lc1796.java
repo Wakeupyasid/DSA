a350242fbd58da974c4414eeb89b5a779079fc79
Wakeupyasid <siddhumore602@gmail.com>
2025-03-30 23:46:42 +0530

    dsa

--- lc1796.java
+++ lc1796.java
@@ -1,13 +1,13 @@
-import java.util.*;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.Scanner;
 
-public class apple {
+public class lc1796 {
+    // 1796. Second Largest Digit in a String
     public static void main(String[] args) {
         String s = "sjhtz8344";
         int ans=-1;
-        List<Integer>a=new ArrayList<>();
+        List<Integer> a=new ArrayList<>();
         for (int i=0;i<s.length();i++){
             if (s.charAt(i)>=48 && s.charAt(i)<=57){
                 int p=Integer.parseInt(String.valueOf(s.charAt(i)));
