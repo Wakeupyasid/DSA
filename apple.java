5d2fc10bd20592b537145b64fbc1cd5f3aa41043
Wakeupyasid <siddhumore602@gmail.com>
2025-03-30 23:16:53 +0530

    dsa

--- apple.java
+++ apple.java
@@ -5,17 +5,19 @@ import java.util.Scanner;
 
 public class apple {
     public static void main(String[] args) {
-        int nums[]={-2};
-        int ans=Integer.MIN_VALUE;
+        int nums[]={1,1,2};
+
+        List<Integer>a=new ArrayList<>();
         for (int i=0;i< nums.length;i++){
-            int c=1;
-            for (int j=i;j< nums.length;j++){
-                c=c*nums[j];
-                if (c>ans){
-                    ans=c;
-                }
+            if (!a.contains(nums[i])){
+                a.add(nums[i]);
             }
         }
+        int ans=a.size();
+        for (int i=0;i< a.size();i++){
+            nums[i]=a.get(i);
+        }
         System.out.println(ans);
+        System.out.println(Arrays.toString(nums));
     }
 }
---
+++ lc26.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class lc26 {
+    public static void main(String[] args) {
+        int nums[] = {1, 1, 2};
+        List<Integer> a = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (!a.contains(nums[i])) {
+                a.add(nums[i]);
+            }
+        }
+        int ans = a.size();
+        for (int i = 0; i < a.size(); i++) {
+            nums[i] = a.get(i);
+        }
+        System.out.println(ans);
+        System.out.println(Arrays.toString(nums));
+    }
+}
