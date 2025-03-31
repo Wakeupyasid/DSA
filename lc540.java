557e3757fe5e27155380401451089929c0539372
Wakeupyasid <siddhumore602@gmail.com>
2025-03-31 23:25:55 +0530

    dsa

--- lc540.java
+++ lc540.java
@@ -1,9 +1,11 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
-public class apple {
+public class lc540 {
+    // 540. Single Element in a Sorted Array
     public static void main(String[] args) {
         int nums[]={3,3,7,7,10,11,11};
-        List<Integer>a=new ArrayList<>();
+        List<Integer> a=new ArrayList<>();
         for (int i=0;i< nums.length;i++){
             if (!a.contains(nums[i])){
                 a.add(nums[i]);
