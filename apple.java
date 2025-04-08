1bb9d69965d448284e88a2dc6a483a160a42c96c
Wakeupyasid <siddhumore602@gmail.com>
2025-04-08 23:46:02 +0530

    dsa

--- apple.java
+++ apple.java
@@ -2,13 +2,11 @@ import java.util.*;
 
 public class apple {
     public static void main(String[] args) {
-        int nums[]={10,100};
+        int nums[]={14,70,53,83,49,91,36,80,92,51,66,70};
         int ans=0;
         for (int i=0;i< nums.length;i++){
             for (int j=i;j< nums.length;j++){
-                if (Math.abs(nums[i]-nums[j])<=Math.min(nums[i],nums[j])){
-                    ans=Math.max(ans,(nums[i]^nums[j]));
-                }
+                ans=Math.max(ans,(nums[i]^nums[j]));
             }
         }
         System.out.println(ans);
