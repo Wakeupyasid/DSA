7d91185b09ab9c1d1042dd0f53140a4f596b30bc
Wakeupyasid <siddhumore602@gmail.com>
2025-04-10 23:45:30 +0530

    dsa

--- apple.java
+++ apple.java
@@ -2,9 +2,19 @@ import java.util.*;
 
 public class apple {
     public static void main(String[] args) {
-        int a=2;
-        int []b={3};
-        int c= (int) (Math.pow(a,b[0])%1337);
-        System.out.println(c);
+        int arr[]={5,1,6};
+        List<Integer>a=new ArrayList<>();
+        for (int i=0;i<arr.length;i++){
+            int s=0;
+            for (int j=i;j<arr.length;j++){
+                s=s^arr[j];
+                a.add(s);
+            }
+        }
+        int ans=0;
+        for (int i=0;i<a.size();i++){
+            ans=ans+a.get(i);
+        }
+        System.out.println(ans);
     }
 }
---
+++ lc2465.java
@@ -0,0 +1,5 @@
+public class lc2465 {
+    public static void main(String[] args) {
+        
+    }
+}
