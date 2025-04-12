8f711f24a6785e28edfecf80075c9101c05b714c
Wakeupyasid <siddhumore602@gmail.com>
2025-04-12 23:09:51 +0530

    dsa

--- lc3079.java
+++ lc3079.java
@@ -1,11 +1,14 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-public class apple {
+public class lc3079 {
+    // 3079. Find the Sum of Encrypted Integers
     public static void main(String[] args) {
         int nums[]={1,2,3};
         int ans=0;
         for (int i=0;i< nums.length;i++){
-            List<Integer>a=new ArrayList<>();
+            List<Integer> a=new ArrayList<>();
             int t=nums[i];
             int m=0;
             while (t>0){
