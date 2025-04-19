3e7046e75cf3168d476a7757cb8645b305c13e5f
Wakeupyasid <siddhumore602@gmail.com>
2025-04-19 23:34:26 +0530

    dsa

--- lc1342.java
+++ lc1342.java
@@ -1,6 +1,5 @@
-import java.util.*;
-
-public class apple {
+public class lc1342 {
+    // 1342. Number of Steps to Reduce a Number to Zero
     public static void main(String[] args) {
         int num=123;
         int ans=0;
