e08ad4cbcb7f5bb87e7e406b7dfe35687f8161b7
Wakeupyasid <siddhumore602@gmail.com>
2025-04-25 23:16:33 +0530

    dsa

--- lc3442.java
+++ lc3442.java
@@ -1,6 +1,8 @@
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
-public class apple {
+public class lc3442 {
+    // 3442. Maximum Difference Between Even and Odd Frequency I
     public static void main(String[] args) {
         String s = "abcabcab";
         int n=s.length();
