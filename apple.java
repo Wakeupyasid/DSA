3abf583eb9dd1e27d2b47dde0929a498acfa8d0c
Wakeupyasid <siddhumore602@gmail.com>
2025-04-30 23:54:36 +0530

    dsa

--- apple.java
+++ apple.java
@@ -2,15 +2,10 @@ import java.util.*;
 
 public class apple {
     public static void main(String[] args) {
-        int nums[]={10};
-        Arrays.sort(nums);
-        int ans=0;
-        if (nums.length>1){
-            for (int i=0;i< nums.length-1;i++){
-                int k=nums[i+1]-nums[i];
-                ans=Math.max(ans,k);
-            }
+        String operations[]={"5","2","C","D","+"};
+        List<Integer>a=new ArrayList<>();
+        for (int i=0;i< operations.length;i++){
+            
         }
-        System.out.println(ans);
     }
 }
