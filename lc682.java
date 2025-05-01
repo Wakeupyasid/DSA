7c388bc5e3abbe2cc6a2904ceafcefcbd9cca802
Wakeupyasid <siddhumore602@gmail.com>
2025-05-01 23:59:28 +0530

    dsa

--- lc682.java
+++ lc682.java
@@ -1,13 +1,11 @@
 import java.util.*;
-
-public class apple {
+public class lc682 {
     public static void main(String[] args) {
         String operations[]={"5","2","C","D","+"};
-        List<Integer>a=new ArrayList<>();
+        List<Integer> a=new ArrayList<>();
         for (int i=0;i< operations.length;i++){
             if (operations[i]=="+"){
                 a.add(a.get(a.size()-1)+a.get(a.size()-2));
-
             }
         }
     }
