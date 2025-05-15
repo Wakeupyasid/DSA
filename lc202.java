d0a120d845b85e30ce54caededbc50047946209e
Wakeupyasid <siddhumore602@gmail.com>
2025-05-15 22:59:19 +0530

    dsa

--- lc202.java
+++ lc202.java
@@ -1,7 +1,5 @@
-import java.math.BigInteger;
-import java.util.*;
-
-public class apple {
+public class lc202 {
+    // 202. Happy Number
     public static void main(String[] args) {
         int n=2;
         boolean ans=false;
