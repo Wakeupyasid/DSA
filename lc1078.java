034d36cad46821c10c374eeec82ee9559f558556
Wakeupyasid <siddhumore602@gmail.com>
2025-05-19 22:56:40 +0530

    dsa

--- lc1078.java
+++ lc1078.java
@@ -1,14 +1,15 @@
-import jdk.jfr.Unsigned;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
-import java.math.BigInteger;
-import java.util.*;
-
-public class apple {
+public class lc1078 {
+    // 1078. Occurrences After Bigram
     public static void main(String[] args) {
         String text = "alice is a good girl she is a good student";
         String first = "a", second = "good";
         String[] f = text.split(" ");
-        List<String>p=new ArrayList<>();
+        List<String> p=new ArrayList<>();
         for (int i=0;i<f.length-2;i++){
             if (Objects.equals(f[i], first) && Objects.equals(f[i + 1], second)){
                 p.add(f[i+2]);
