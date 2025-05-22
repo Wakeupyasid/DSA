99b4e8b392b39b5f7e8b10af1deca11a6b30af40
Wakeupyasid <siddhumore602@gmail.com>
2025-05-22 23:54:23 +0530

    dsa

--- apple.java
+++ apple.java
@@ -5,15 +5,10 @@ import java.util.*;
 
 public class apple {
     public static void main(String[] args) {
-        int nums[]={2,5,6,0,0,1,2};
-        int target=0;
-        boolean ans=false;
-        for (int i=0;i< nums.length;i++){
-            if (nums[i]==target){
-                ans=true;
-                break;
-            }
+        int n=10;
+        int ans=0;
+        if (n>=10){
+
         }
-        System.out.println(ans);
     }
 }
