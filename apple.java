22f9fe844f87892c3f758aee2376a4679d0588cb
Wakeupyasid <siddhumore602@gmail.com>
2025-05-29 23:18:42 +0530

    dsa

--- apple.java
+++ apple.java
@@ -3,17 +3,9 @@ import java.math.BigInteger;
 import java.util.*;
 public class apple {
     public static void main(String[] args) {
-        String words[]={"a","aba","ababa","aa"};
-        int ans=0;
-        for (int i=0;i<words.length-1;i++){
-            for (int j=i+1;j< words.length;j++){
-                if (words[i].length()<words[j].length()){
-                    if (words[j].startsWith(words[i]) && words[j].endsWith(words[i])){
-                        ans++;
-                    }
-                }
-            }
-        }
-        System.out.println(ans);
+        String s = "LeetcodeHelpsMeLearn";
+        int spaces[]={8,13,15};
+        int k=0;
+
     }
 }
