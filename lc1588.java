ec959f61110f9f5a91e717565048c52a7ff02d91
Wakeupyasid <siddhumore602@gmail.com>
2025-06-06 14:32:47 +0530

    dsa

--- lc1588.java
+++ lc1588.java
@@ -1,12 +1,13 @@
-import jdk.jfr.Unsigned;
-import java.math.BigInteger;
-import java.util.*;
-public class apple {
+import java.util.ArrayList;
+import java.util.List;
+
+public class lc1588 {
+    // 1588. Sum of All Odd Length Subarrays
     public static void main(String[] args) {
         int arr[]={1,2};
         int ans=0;
         for (int i=0;i<arr.length;i++){
-            List<Integer>a=new ArrayList<>();
+            List<Integer> a=new ArrayList<>();
             int k=0;
             for (int j=i;j<arr.length;j++){
                 k=k+arr[j];
