8f153a37aa919c5ae60b500c369d304540d55a6a
Wakeupyasid <siddhumore602@gmail.com>
2025-06-07 23:27:39 +0530

    dsa

--- lc1422.java
+++ lc1422.java
@@ -1,7 +1,5 @@
-import jdk.jfr.Unsigned;
-import java.math.BigInteger;
-import java.util.*;
-public class apple {
+public class lc1422 {
+    // 1422. Maximum Score After Splitting a String
     public static void main(String[] args) {
         String s = "00111";
         int ans=Integer.MIN_VALUE;
