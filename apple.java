273bec33edb008dffffd5c581d9c6aa38acd1406
Wakeupyasid <siddhumore602@gmail.com>
2025-06-09 23:20:56 +0530

    dsa

--- apple.java
+++ apple.java
@@ -3,28 +3,7 @@ import java.math.BigInteger;
 import java.util.*;
 public class apple {
     public static void main(String[] args) {
-        int nums[]={4,2,5,7};
-        List<Integer>o=new ArrayList<>();
-        List<Integer>e=new ArrayList<>();
-        for (int i=0;i< nums.length;i++){
-            if (nums[i]%2==0){
-                e.add(nums[i]);
-            }
-            else{
-                o.add(nums[i]);
-            }
-        }
-        int a=0;
-        int b=0;
-        int ans[]=new int[nums.length];
-        for (int i=0;i<ans.length;i++){
-            if (i%2==0){
-                ans[i]=e.get(a++);
-            }
-            else{
-                ans[i]=o.get(b++);
-            }
-        }
-        System.out.println(Arrays.toString(ans));
+        int nums[]={1,12,-5,-6,50,3};
+        
     }
 }
