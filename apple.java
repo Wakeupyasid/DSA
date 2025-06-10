4c28e6ebb3833078eebcbbdca24a60362e7538d1
Wakeupyasid <siddhumore602@gmail.com>
2025-06-10 23:26:37 +0530

    dsa

--- apple.java
+++ apple.java
@@ -3,7 +3,18 @@ import java.math.BigInteger;
 import java.util.*;
 public class apple {
     public static void main(String[] args) {
-        int nums[]={1,12,-5,-6,50,3};
-        
+        int nums[]={1,3,5,6};
+        int target=7;
+        int ans=-1;
+        for (int i=0;i< nums.length;i++){
+            if (nums[i]>=target){
+                ans=i;
+                break;
+            }
+        }
+        if (ans==-1){
+            ans= nums.length;
+        }
+        System.out.println(ans);
     }
 }
--- lc35.java
+++ lc35.java
@@ -2,22 +2,16 @@ public class lc35 {
     // 35. Search Insert Position
     public static void main(String[] args) {
         int nums[]={1,3,5,6};
-        int target=2;
-        int ans=0;
-        if (nums[nums.length-1]<target){
-            ans=nums.length;
-        }
-        else {
-            for (int i=0;i< nums.length;i++){
-                if (nums[i]==target){
-                    ans=i;
-                    break;
-                }
-                else if (nums[i]>target){
-                    ans=i;
-                    break;
-                }
+        int target=7;
+        int ans=-1;
+        for (int i=0;i< nums.length;i++){
+            if (nums[i]>=target){
+                ans=i;
+                break;
             }
+        }
+        if (ans==-1){
+            ans= nums.length;
         }
         System.out.println(ans);
     }
