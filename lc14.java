7f0a5bb215d31a62369f472e97b2989f853c7763
Wakeupyasid <siddhumore602@gmail.com>
2025-06-23 23:23:11 +0530

    dsa

---
+++ lc14.java
@@ -0,0 +1,20 @@
+public class lc14 {
+
+    // 14. Longest Common Prefix
+
+    public static String longestCommonPrefix(String[] strs) {
+        int numberOfStrings = strs.length;
+        for (int index = 0; index < strs[0].length(); ++index) {
+            for (int stringIndex = 1; stringIndex < numberOfStrings; ++stringIndex) {
+                if (strs[stringIndex].length() <= index || strs[stringIndex].charAt(index) != strs[0].charAt(index)) {
+                    return strs[0].substring(0, index);
+                }
+            }
+        }
+        return strs[0];
+    }
+    public static void main(String[] args) {
+        String strs[]={"flower","flow","flight"};
+        System.out.println(longestCommonPrefix(strs));
+    }
+}
