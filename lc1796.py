fe2e857fb5661ffe125475ecf9bccb15b6cfdf70
Wakeupyasid <siddhumore602@gmail.com>
2025-07-27 23:47:14 +0530

    dsa

---
+++ lc1796.py
@@ -0,0 +1,14 @@
+class Solution:
+    def secondHighest(self, s: str) -> int:
+        maxDigit = -1
+        secondMaxDigit = -1
+        for c in s:
+            if c.isdigit():
+                d = int(c)
+                if d > maxDigit:
+                    secondMaxDigit = maxDigit
+                    maxDigit = d
+                elif maxDigit > d > secondMaxDigit:
+                    secondMaxDigit = d
+        return secondMaxDigit
+print(Solution().secondHighest("0123456789"))
