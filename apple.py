d3672c8189602152c24f8c14ad1319181607c8e8
Wakeupyasid <siddhumore602@gmail.com>
2025-07-28 23:44:31 +0530

    dsa

--- apple.py
+++ apple.py
@@ -1,2 +1,13 @@
-for i in range(1,):
-    print(i)
+from sympy import false
+
+n=4
+c=0
+for i in range(1,n+1):
+    if n%i==0:
+        c+=1
+    if c==3:
+        break
+ans=False
+if c==3:
+    ans=True
+print(ans)
