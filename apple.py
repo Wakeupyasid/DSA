21a608514d860ef629ce88b49db5e247d5d5844c
Wakeupyasid <siddhumore602@gmail.com>
2025-07-31 23:21:00 +0530

    dsa

--- apple.py
+++ apple.py
@@ -1,2 +1,6 @@
-s="Hello"
-print(s.lower())
+nums = [-4,-1,0,3,10]
+ans=[]
+for i in nums:
+    ans.append(i*i)
+ans.sort()
+print(ans)
---
+++ lc977.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        ans=[]
+        for i in nums:
+            ans.append(i*i)
+        ans.sort()
+        return ans
