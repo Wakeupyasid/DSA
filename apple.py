58efd8a132d006655215cc5d5b4e8eebf4c57c9b
Wakeupyasid <siddhumore602@gmail.com>
2025-08-11 23:52:55 +0530

    dsa

--- apple.py
+++ apple.py
@@ -1,10 +1,23 @@
-import collections
+from typing import List
 
-nums = [0,1,0,1,0,1,99]
-d=dict(collections.Counter(nums))
-a=0
-for i,v in d.items():
-    if v==1:
-        a=i
-        break
-print(a)
+# int ans=0;
+#         for (int i=0;i<nums.length-1;i++){
+#             for (int j=i+1;j< nums.length;j++){
+#                 if ((nums[i] == nums[j]) && (i<j)){
+#                     ans++;
+#                 }
+#             }
+#         }
+#         return ans;
+nums=[1,2,3,1,1,3]
+for i, v in enumerate(nums):
+    for j in range(i + 1, len(nums)):
+        print(j, end=" ")
+    print(end="\n")
+
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        ans=0
+        for i,v in enumerate(nums):
+            for j in range(i+1,len(nums)):
+                print(j, end=" ")
