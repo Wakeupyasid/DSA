ddd05d5a78fd69faf2e6abf739d70396d037fda7
Wakeupyasid <siddhumore602@gmail.com>
2025-09-17 23:54:29 +0530

    dsa

--- apple.py
+++ apple.py
@@ -1,6 +1,3 @@
-nums = [1,1,2,3]
-l=[]
-for i in range(1,len(nums),2):
-    for j in range (0,nums[i-1]):
-        l.append(nums[i])
-print(l)
+arr = [17,18,5,4,6,1]
+for i in range (0,len(arr)):
+    print(arr[i:len(arr)])
