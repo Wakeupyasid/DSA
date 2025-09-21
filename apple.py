cba3f555134c265d1b0dc88d3ec4a0d41eab2b82
Wakeupyasid <siddhumore602@gmail.com>
2025-09-21 23:36:14 +0530

    dsa

--- apple.py
+++ apple.py
@@ -1,3 +1,6 @@
 arr = [17,18,5,4,6,1]
-for i in range (0,len(arr)):
-    print(arr[i:len(arr)])
+ans=[]
+for i in range(0,len(arr)-1):
+    ans.append(max(arr[i+1:len(arr)]))
+    print(sum(arr[i+1:len(arr)]))
+ans.append(-1)
