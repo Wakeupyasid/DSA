de9e9ba88de880cb8e29001702d5f5b57aeb019e
Wakeupyasid <siddhumore602@gmail.com>
2025-09-25 23:54:53 +0530

    dsa

--- apple.py
+++ apple.py
@@ -1,7 +1,2 @@
-nums = [4,2,2,3,1]
-unique_nums = sorted(set(nums), reverse=True)
-if len(unique_nums) >= 3:
-    third_largest = unique_nums[2]
-    print("Third largest number:", third_largest)
-else:
-    print("Less than 3 unique numbers")
+num=100
+print()
