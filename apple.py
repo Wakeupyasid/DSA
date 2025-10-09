75a0b0fe6c1c554401c1590d50c76c9fe69ba77c
Wakeupyasid <siddhumore602@gmail.com>
2025-10-09 23:47:04 +0530

    dsa

--- apple.py
+++ apple.py
@@ -1,12 +1,2 @@
-num = 443
-ans=False
-if num==0:
-    ans=True
-else:
-    for i in range(num):
-        a=str(i)
-        b=a[::-1]
-        if int(a)+int(b)==num:
-            ans=True
-            break
-print(ans)
+n = 723344511
+ans=[]
