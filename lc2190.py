944b84ceae1fed26cf169750936be06329b1edcd
Wakeupyasid <siddhumore602@gmail.com>
2025-10-10 23:54:57 +0530

    dsa

---
+++ lc2190.py
@@ -0,0 +1,13 @@
+import collections
+import itertools
+
+
+class Solution:
+  def mostFrequent(self, nums: list[int], key: int) -> int:
+    count = collections.Counter()
+
+    for a, b in itertools.pairwise(nums):
+      if a == key:
+        count[b] += 1
+
+    return max(count, key=lambda num: count[num])
