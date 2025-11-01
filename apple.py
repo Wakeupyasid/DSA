06a81d8d4d40e6ac2188e87ac7553ee888e13417
Wakeupyasid <siddhumore602@gmail.com>
2025-11-01 22:49:10 +0530

    dsa

--- apple.py
+++ apple.py
@@ -1,11 +1,9 @@
-nums = [9,7,3]
-k = 5
-ans=0
-s=sum(nums)
-if s>k & s%k!=0:
-    while True:
-        s=s-1
-        ans+=1
-        if s%k==0:
-            break
+n=5
+t=bin(n)[2:]
+ans=[0,0]
+for i in range(0,len(t)):
+    if (t[i]=='1') & (i%2==0):
+        ans[1]+=1
+    elif (t[i]=='1') & (i%2!=0):
+        ans[0]+=1
 print(ans)
