52446d38ad2ebe782fbcc61cab6e9e08229263bc
Wakeupyasid <siddhumore602@gmail.com>
2025-11-02 12:35:19 +0530

    dsa

--- apple.py
+++ apple.py
@@ -1,9 +1,9 @@
-n=5
-t=bin(n)[2:]
-ans=[0,0]
-for i in range(0,len(t)):
-    if (t[i]=='1') & (i%2==0):
-        ans[1]+=1
-    elif (t[i]=='1') & (i%2!=0):
-        ans[0]+=1
+s = "cbbd"
+ans=""
+for i in range(len(s)):
+    for j in range(i + 1, len(s) + 1):
+        t=s[i:j]
+        k=t[::-1]
+        if k == t and len(t)>len(ans):
+            ans=t
 print(ans)
---
+++ lc5.py
@@ -0,0 +1,10 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        ans=""
+        for i in range(len(s)):
+            for j in range(i + 1, len(s) + 1):
+                t=s[i:j]
+                k=t[::-1]
+                if k == t and len(t)>len(ans):
+                    ans=t
+        return ans
