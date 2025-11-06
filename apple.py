887bf4be075881f502e2c1f8adcba85aa1437fb3
Wakeupyasid <siddhumore602@gmail.com>
2025-11-06 23:57:00 +0530

    dsa

--- apple.py
+++ apple.py
@@ -1,14 +1,2 @@
-import numpy as np
-n=4
-lst = [i for i in range(1,n+1)]
-ans=-1
-if n==1:
-    ans=1
-else:
-    for i in range(len(lst) - 1):
-        pre = lst[0:i + 1]
-        post = lst[i:]
-        if sum(pre) == sum(post):
-            ans = lst[i]
-            break
-print(ans)
+s=[]
+s
