f37e07527953317cd35e02a759c10acac54e0e8a
Wakeupyasid <siddhumore602@gmail.com>
2025-11-18 23:37:17 +0530

    dsa

--- apple.py
+++ apple.py
@@ -1,6 +1,7 @@
-s = "aab"
-ans=[[]]
-for i in range(0,len(s)):
-    for j in range(0,len(s)):
-        print(s[i:j+1],s[i:j+1:-1],end="\n")
+nums = [1,1]
+ans=[]
+for i in range(0,len(nums)-1):
+    if nums[i] == nums[i+1]:
+        ans.append(nums[i])
+        break
 print(ans)
