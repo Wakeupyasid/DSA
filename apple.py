7aa3f605c325cf8add85f6594049e4b089b6c7a1
Wakeupyasid <siddhumore602@gmail.com>
2025-12-05 13:36:57 +0530

    dsa

--- apple.py
+++ apple.py
@@ -1,5 +1,16 @@
-s = "ababa"
-result = []
-for length in range(len(s), 0, -1):
-    for start in range(0, len(s) - length + 1):
-        result.append(s[start:start+length])
+import math
+nums = [10,4,8,3]
+n = len(nums)
+
+left_sum = [0] * n
+right_sum = [0] * n
+
+for i in range(1, n):
+    left_sum[i] = left_sum[i-1] + nums[i-1]
+
+for i in range(n-2, -1, -1):
+    right_sum[i] = right_sum[i+1] + nums[i+1]
+ans=[]
+for i in range(len(left_sum)):
+    ans.append(abs(left_sum[i] - right_sum[i]))
+print(ans)
