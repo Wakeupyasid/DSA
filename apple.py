e472a880824c887a8b352ab63b46063e5e8cc2af
Wakeupyasid <siddhumore602@gmail.com>
2025-12-06 13:33:14 +0530

    dsa

--- apple.py
+++ apple.py
@@ -1,16 +1,8 @@
-import math
-nums = [10,4,8,3]
-n = len(nums)
-
-left_sum = [0] * n
-right_sum = [0] * n
-
-for i in range(1, n):
-    left_sum[i] = left_sum[i-1] + nums[i-1]
-
-for i in range(n-2, -1, -1):
-    right_sum[i] = right_sum[i+1] + nums[i+1]
-ans=[]
-for i in range(len(left_sum)):
-    ans.append(abs(left_sum[i] - right_sum[i]))
-print(ans)
+n = 10
+k=0
+ans=0
+for i in range(1,n+1):
+    if i%7==0:
+        k+=1
+    ans=ans+(i+k)
+    print(i+k)
