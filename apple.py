e0844f9b69aa4b2034dad83feb77b9068beb8efa
Wakeupyasid <siddhumore602@gmail.com>
2025-12-13 19:56:12 +0530

    dsa

--- apple.py
+++ apple.py
@@ -1,19 +1,12 @@
-nums = [1,3,2]
-t=[]
-t.append(nums[0])
-for i in nums:
-    if i!=t[-1]:
-        t.append(i)
-ans=True
-if len(t)>1:
-    if t[0]>t[1]:
-        for i in range(len(t)-1):
-            if t[i]<t[i+1]:
-                ans=False
-                break
-    else:
-        for i in range(len(t)-1):
-            if t[i]>t[i+1]:
-                ans=False
-                break
-print(ans)
+from typing import List
+
+
+class Solution:
+    def divideString(self, s: str, k: int, fill: str) -> List[str]:
+        result = []
+        for i in range(0, len(s), k):
+            group = s[i:i + k]
+            if len(group) < k:
+                group += fill * (k - len(group))
+            result.append(group)
+        return result
