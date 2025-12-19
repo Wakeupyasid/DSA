fad66e37b2494e35d185442284b27d11157cea19
Wakeupyasid <siddhumore602@gmail.com>
2025-12-19 15:23:50 +0530

    dsa

--- apple.py
+++ apple.py
@@ -1,10 +1,2 @@
-nums = [1,2,10,5,7, 1]
-ans=True
-c=0
-for i in range (len(nums)-1):
-    if nums[i] > nums[i+1]:
-        c=c+1
-    if c==2:
-        ans=False
-        break
-print(ans)
+s = "iloveleetcode"
+words = ["i","love","leetcode","apples"]
