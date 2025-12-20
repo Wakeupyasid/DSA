6ea16ecdb8be20ab93d8ef65325f20788c797007
Wakeupyasid <siddhumore602@gmail.com>
2025-12-20 12:15:01 +0530

    dsa

--- apple.py
+++ apple.py
@@ -1,2 +1,14 @@
-s = "iloveleetcode"
-words = ["i","love","leetcode","apples"]
+nums1 = [1,2,3]
+nums2 = [2,4,6]
+ans=[]
+n1=[]
+n2=[]
+for i in nums1:
+    if i not in nums2:
+        n1.append(i)
+for i in nums2:
+    if i not in nums1:
+        n2.append(i)
+ans.append(n1)
+ans.append(n2)
+print(ans)
---
+++ lc2215.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+        ans=[]
+        n1=[]
+        n2=[]
+        for i in nums1:
+            if i not in nums2 and i not in n1:
+                n1.append(i)
+        for i in nums2:
+            if i not in nums1 and i not in n2:
+                n2.append(i)
+        ans.append(n1)
+        ans.append(n2)
+        return ans
