937b428080b97fb0dbd07ff9297fc4d0a3fad246
Wakeupyasid <siddhumore602@gmail.com>
2025-12-21 13:56:37 +0530

    dsa

--- apple.py
+++ apple.py
@@ -1,14 +1,9 @@
-nums1 = [1,2,3]
-nums2 = [2,4,6]
-ans=[]
-n1=[]
-n2=[]
-for i in nums1:
-    if i not in nums2:
-        n1.append(i)
-for i in nums2:
-    if i not in nums1:
-        n2.append(i)
-ans.append(n1)
-ans.append(n2)
+nums = [5,5,5]
+k = 2
+nums.sort()
+ans=0
+while k!=0:
+    ans+=nums[-1]
+    nums[-1]=nums[-1]+1
+    k-=1
 print(ans)
