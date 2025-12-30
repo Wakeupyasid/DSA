80a2b43749cf2e02b9d3a10804408f91fc0b2e75
Wakeupyasid <siddhumore602@gmail.com>
2025-12-30 13:23:18 +0530

    dsa

--- apple.py
+++ apple.py
@@ -1,9 +1,9 @@
-nums = [2,1,-1]
+haystack = "leetcode"
+needle = "leeto"
 ans=-1
-for i in range(0,len(nums)):
-    a=sum(nums[0:i])
-    b=sum(nums[i+1:])
-    if a==b:
-        ans=i
-        break
+if needle in haystack:
+    for i in range(0, len(haystack)-len(needle)+1):
+        if haystack[i:i+len(needle)]==needle:
+            ans=i
+            break
 print(ans)
---
+++ lc28.py
@@ -0,0 +1,9 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        ans=-1
+        if needle in haystack:
+            for i in range(0, len(haystack)-len(needle)+1):
+                if haystack[i:i+len(needle)]==needle:
+                    ans=i
+                    break
+        return ans
